cd67e6004bf43183272f20030b72236557d8d095
Jaren Glenn <jarenglenn@gmail.com>
2025-07-20 16:45:25 -0600

    feat(config/coding): enable mini.pairs

---
+++ config/coding/mini-autopairs.nix
@@ -0,0 +1,16 @@
+{
+  vim.mini.pairs = {
+    enable = true;
+    setupOpts = {
+      modes = {
+        insert = true;
+        command = false;
+        terminal = false;
+      };
+      skip_next = ''[=[[%w%%%'%[%"%.%`%$]]=]'';
+      skip_ts = ["string"];
+      skip_unbalanced = true;
+      markdown = true;
+    };
+  };
+}
