d13a7eb2ad670c98fcaf294071e618a095da28b1
Jaren Glenn <jarenglenn@gmail.com>
2025-07-20 14:56:05 -0600

    refactor(packages): automatically call all packages

--- packages/default.nix
+++ packages/default.nix
@@ -1,7 +1,19 @@
 {
   pkgs,
   inputs,
+  lib,
   ...
-}: {
-  close-buffers-nvim = pkgs.callPackage ./close-buffers-nvim.nix {inherit inputs;};
-}
+}: let
+  files =
+    builtins.filter (name: name != "default.nix")
+    (builtins.attrNames (builtins.readDir ./.));
+
+  names = map (name: lib.removeSuffix ".nix" name) files;
+
+  packages = builtins.listToAttrs (map (name: {
+      name = name;
+      value = pkgs.callPackage (./. + "/${name}.nix") {inherit inputs;};
+    })
+    names);
+in
+  packages
