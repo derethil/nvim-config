5c220b9a5ea6ff05acb8e9a046b6d1bf21604f71
Jaren Glenn <jarenglenn@gmail.com>
2025-07-20 16:43:31 -0600

    feat(config/coding): configure flash.nvim

---
+++ config/coding/flash.nix
@@ -0,0 +1,12 @@
+{
+  vim.utility.motion.flash-nvim = {
+    enable = true;
+    mappings = {
+      jump = "s";
+      treesitter = "S";
+      remote = "r";
+      treesitter_search = "R";
+      toggle = "<C-s>";
+    };
+  };
+}
