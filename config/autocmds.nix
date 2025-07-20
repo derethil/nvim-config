cf8172daf38fb2b0035765b8b287d70528569ae5
Jaren Glenn <jarenglenn@gmail.com>
2025-07-19 23:08:23 -0600

    feat(autocmds): highlight yanked text

---
+++ config/autocmds.nix
@@ -0,0 +1,14 @@
+{ lib, ... }:
+{
+  vim.autocmds = [
+    {
+      event = ["TextYankPost"];
+      desc = "Highlight when yanking (copying) text";
+      callback = lib.generators.mkLuaInline ''
+        function()
+          vim.hl.on_yank()
+        end
+      '';
+    }
+  ];
+}
