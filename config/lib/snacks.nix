14557e0d0ce19b1a27856e1a0217c777f3b5fc84
Jaren Glenn <jarenglenn@gmail.com>
2025-07-20 14:28:08 -0600

    feat(lib): snacks

---
+++ config/lib/snacks.nix
@@ -0,0 +1,20 @@
+{
+  vim.utility.snacks-nvim = {
+    enable = true;
+    setupOpts = {
+      rename = {enabled = true;};
+      bigfile = {enabled = false;};
+      dashboard = {enabled = false;};
+      explorer = {enabled = false;};
+      indent = {enabled = false;};
+      input = {enabled = false;};
+      picker = {enabled = false;};
+      notifier = {enabled = false;};
+      quickfile = {enabled = false;};
+      scope = {enabled = false;};
+      scroll = {enabled = false;};
+      statuscolumn = {enabled = true;};
+      words = {enabled = false;};
+    };
+  };
+}
