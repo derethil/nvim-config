70f00132ccdd17d32fd89b7e7a6e635079ab3042
Jaren Glenn <jarenglenn@gmail.com>
2025-07-20 14:27:41 -0600

    feat(ui): cursorline

---
+++ config/ui/cursorline.nix
@@ -0,0 +1,11 @@
+{
+  vim.visuals.nvim-cursorline = {
+    enable = true;
+    setupOpts = {
+      cursorline.enable = true;
+      cursorline.timeout = 0;
+      cursorword.enable = true;
+      cursorword.timeout = 0;
+    };
+  };
+}
