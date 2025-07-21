4514634e2902e679bd8054c4ba725f29b2b9ae1b
Jaren Glenn <jarenglenn@gmail.com>
2025-07-21 13:47:25 -0600

    feat(keymaps): add some basic keymaps

--- config/keymaps.nix
+++ config/keymaps.nix
@@ -7,5 +7,41 @@
       silent = true;
       desc = "Clear search highlights";
     }
+    {
+      key = "<leader>qq";
+      mode = ["n"];
+      action = "<CMD>qa<CR>";
+      desc = "Quit All";
+    }
+    {
+      key = "<leader>q";
+      mode = ["n"];
+      action = "<CMD>q<CR>";
+      desc = "Quit";
+    }
+    {
+      key = "x";
+      mode = ["n"];
+      action = "\"_x";
+      desc = "Delete without yanking";
+    }
+    {
+      key = "p";
+      mode = ["v"];
+      action = "\"_dP";
+      desc = "Paste without yanking";
+    }
+    {
+      key = "<leader>j";
+      mode = ["n"];
+      action = "*``cgn";
+      desc = "Replace word under cursor";
+    }
+    {
+      key = "<C-c>";
+      mode = ["n" "v" "i"];
+      action = "<ESC>ggVGy";
+      desc = "Yank all";
+    }
   ];
 }
