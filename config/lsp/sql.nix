0c3a6756d9715347e27b960df336edc5106568bf
Jaren Glenn <jarenglenn@gmail.com>
2025-07-23 14:32:18 -0600

    feat(config/lsp): enable sql support

---
+++ config/lsp/sql.nix
@@ -0,0 +1,12 @@
+{
+  vim.languages.sql = {
+    enable = true;
+    lsp.enable = true;
+    lsp.server = "sqls";
+    treesitter.enable = true;
+    format = {
+      enable = true;
+    };
+  };
+}
+
