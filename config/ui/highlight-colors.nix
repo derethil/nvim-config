5dd04774b1a80c685dd275e77785ea161369743b
Jaren Glenn <jarenglenn@gmail.com>
2025-07-23 17:20:53 -0600

    feat(config/ui): configure nvim-highlight-colors

---
+++ config/ui/highlight-colors.nix
@@ -0,0 +1,18 @@
+{
+  pkgs,
+  lib,
+  ...
+}: {
+  vim.lazy.plugins."nvim-highlight-colors" = {
+    package = pkgs.vimPlugins.nvim-highlight-colors;
+    event = [lib.events.VeryLazy];
+    ft = ["typescriptreact" "javascriptreact" "css" "javascript" "typescript" "html"];
+    setupModule = "nvim-highlight-colors";
+    setupOpts = {
+      render = "virtual";
+      virtual_symbol = "";
+      enable_named_colors = true;
+      enable_tailwind = true;
+    };
+  };
+}
