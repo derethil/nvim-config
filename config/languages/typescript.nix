d1088a1a6b32f61422a3528263390dff29b42a32
Jaren Glenn <jarenglenn@gmail.com>
2025-07-23 21:36:12 -0600

    feat(config/typescript): add some file icons

--- config/languages/typescript.nix
+++ config/languages/typescript.nix
@@ -1,4 +1,4 @@
-{pkgs}: {
+{
   vim.languages.ts = {
     enable = true;
     lsp.enable = true;
@@ -7,10 +7,50 @@
     format = {
       enable = true;
       type = "prettierd";
-      package = pkgs.prettierd;
     };
     extensions = {
       ts-error-translator.enable = true;
+    };
+  };
+
+  vim.mini.icons.setupOpts = {
+    file = {
+      ".eslintrc.js" = {
+        glyph = "󰱺";
+        hl = "MiniIconsYellow";
+      };
+      ".node-version" = {
+        glyph = "";
+        hl = "MiniIconsGreen";
+      };
+      ".prettierrc" = {
+        glyph = "";
+        hl = "MiniIconsPurple";
+      };
+      ".yarnrc.yml" = {
+        glyph = "";
+        hl = "MiniIconsBlue";
+      };
+      "eslint.config.js" = {
+        glyph = "󰱺";
+        hl = "MiniIconsYellow";
+      };
+      "package.json" = {
+        glyph = "";
+        hl = "MiniIconsGreen";
+      };
+      "tsconfig.json" = {
+        glyph = "";
+        hl = "MiniIconsAzure";
+      };
+      "tsconfig.build.json" = {
+        glyph = "";
+        hl = "MiniIconsAzure";
+      };
+      "yarn.lock" = {
+        glyph = "";
+        hl = "MiniIconsBlue";
+      };
     };
   };
 }
