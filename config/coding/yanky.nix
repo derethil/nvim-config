97141af4f2e3d6afc23d09d0c963ae05b36a0582
Jaren Glenn <jarenglenn@gmail.com>
2025-07-23 18:42:10 -0600

    feat(config/yanky): yank history

--- config/coding/yanky.nix
+++ config/coding/yanky.nix
@@ -9,13 +9,46 @@
     };
   };
 
+  vim.lazy.plugins = {
+    plenary-nvim = {
+      package = "plenary-nvim";
+      priority = 100;
+    };
+    telescope = {
+      package = "telescope";
+      setupModule = "telescope";
+      setupOpts = {
+        defaults = {
+          layout_strategy = "flex";
+          sorting_strategy = "ascending";
+          layout_config = {
+            vertical = {
+              prompt_position = "top";
+              mirror = true;
+            };
+            horizontal = {
+              prompt_position = "top";
+              preview_width = 0.55;
+            };
+            flex = {
+              flip_columns = 150;
+            };
+          };
+        };
+      };
+      keys = [
+        {
+          key = "<leader>p";
+          mode = ["n" "x"];
+          action = "function() require('yanky.telescope.yank_history').yank_history() end";
+          lua = true;
+          desc = "Open Yank History";
+        }
+      ];
+    };
+  };
+
   vim.keymaps = [
-    {
-      key = "<leader>p";
-      mode = ["n" "x"];
-      action = "<CMD>YankyRingHistory<CR>"; # TODO: integrate with telescope
-      desc = "Open Yank History";
-    }
     {
       key = "y";
       mode = ["n" "x"];
