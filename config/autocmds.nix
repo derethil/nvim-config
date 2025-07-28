67499ea42baf75c3f41360475a93d9e091fac670
Jaren Glenn <jarenglenn@gmail.com>
2025-07-28 13:23:23 -0600

    fix(config/autocmds): close floating windows on buffer switch

--- config/autocmds.nix
+++ config/autocmds.nix
@@ -1,5 +1,4 @@
-{ lib, ... }:
-{
+{lib, ...}: {
   vim.autocmds = [
     {
       event = ["TextYankPost"];
@@ -7,6 +6,22 @@
       callback = lib.generators.mkLuaInline ''
         function()
           vim.hl.on_yank()
+        end
+      '';
+    }
+    # NOTE: this seems like a bug somewhere, try removing eventually to see if it's fixed
+    {
+      event = ["BufLeave" "WinLeave"];
+      desc = "Close Floating Windows on Buffer Switch";
+      callback = lib.generators.mkLuaInline ''
+        function()
+          for _, win in ipairs(vim.api.nvim_list_wins()) do
+            if vim.api.nvim_win_is_valid(win) then
+              if vim.api.nvim_win_get_config(win).relative == "win" then
+                vim.api.nvim_win_close(win, false)
+              end
+            end
+          end
         end
       '';
     }
