7c4f0c5801461dfb197b36c3724396cd9a2d3838
Jaren Glenn <jarenglenn@gmail.com>
2025-07-29 02:03:57 -0600

    feat(config/languages): qml support

---
+++ config/languages/qml.nix
@@ -0,0 +1,16 @@
+{pkgs, ...}: {
+  vim.extraPackages = with pkgs; [
+    kdePackages.qtdeclarative
+  ];
+
+  vim.lsp.servers = {
+    qmlls = {
+      filetypes = ["qml"];
+      rootPatterns = [".qmlls.ini"];
+    };
+  };
+
+  vim.treesitter.grammars = with pkgs; [
+    vimPlugins.nvim-treesitter-parsers.qmljs
+  ];
+}
