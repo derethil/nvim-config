def75cd5b8595e53f4331b1943697d3b82adc740
Jaren Glenn <jarenglenn@gmail.com>
2025-08-15 17:17:51 -0600

    fix(config/asisstant): fix claude-fzf errors with stay-centered

--- config/coding/stay-centered.nix
+++ config/coding/stay-centered.nix
@@ -11,4 +11,32 @@
       allow_scroll_move = false; # workaround for https://github.com/arnamak/stay-centered.nvim/issues/23
     };
   };
+
+  # Fixes errors on opening claude-code terminal mode from claude-fzf
+  vim.autocmds = [
+    {
+      event = ["TermOpen"];
+      desc = "Disable stay-centered in terminal buffers";
+      callback = lib.generators.mkLuaInline ''
+        function()
+          local stay_centered = require('stay-centered')
+          if stay_centered.enabled then
+            stay_centered.toggle()
+          end
+        end
+      '';
+    }
+    {
+      event = ["TermClose"];
+      desc = "Re-enable stay-centered when leaving terminal buffers";
+      callback = lib.generators.mkLuaInline ''
+        function()
+          local stay_centered = require('stay-centered')
+          if not stay_centered.enabled then
+            stay_centered.toggle()
+          end
+        end
+      '';
+    }
+  ];
 }
