80d89b01fae20f50693098a0835d2eb5d8b855e1
Jaren Glenn <jarenglenn@gmail.com>
2025-08-17 14:52:06 -0600

    feat(config/sonarlint): update to new connectedMode in official plugin

--- flake/modules/options.nix
+++ flake/modules/options.nix
@@ -17,6 +17,70 @@ in {
       };
     };
 
+    sonarlint = {
+      enable = mkEnableOption "Enable SonarLint integration";
+
+      languageServerPackage = mkOption {
+        type = types.package;
+        default = pkgs.sonarlint-ls;
+        description = "Package for SonarLint language server";
+      };
+
+      connectedMode = {
+        enable = mkEnableOption "Enable SonarLint connected mode";
+
+        tokenFile = mkOption {
+          type = types.path;
+          default = "";
+          description = "Path to the file containing the SonarQube token";
+        };
+
+        sonarqubeConnections = mkOption {
+          type = types.listOf types.attrs;
+          default = [];
+          description = "List of SonarQube connections for SonarLint";
+          example = literalExpression ''
+            [
+              {
+                connectionId = "company";
+                serverUrl = "https://sonarqube.company.com";
+                disableNotifications = false;
+              }
+            ]
+          '';
+        };
+
+        projects = mkOption {
+          type = types.attrsOf (types.submodule {
+            options = {
+              connectionId = mkOption {
+                type = types.str;
+                description = "Connection ID to use for this project";
+              };
+              projectKey = mkOption {
+                type = types.str;
+                description = "SonarQube project key";
+              };
+            };
+          });
+          default = {};
+          description = "Map of project paths to their SonarLint configuration";
+          example = literalExpression ''
+            {
+              "/path/to/project1" = {
+                connectionId = "company";
+                projectKey = "project1-sonarqube-key";
+              };
+              "/path/to/project2" = {
+                connectionId = "local-sonar";
+                projectKey = "project2-key";
+              };
+            }
+          '';
+        };
+      };
+    };
+
     defaultEditor = mkOption {
       type = types.bool;
       default = false;
