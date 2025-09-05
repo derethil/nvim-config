dcdd17522a40da96f618e646fcfc263f993d51d7
Jaren Glenn <jarenglenn@gmail.com>
2025-09-05 14:29:56 -0600

    feat(lsp/tailwind): add lsp support for tailwind-variants

--- config/languages/tailwind.nix
+++ config/languages/tailwind.nix
@@ -1,6 +1,42 @@
-{
+{pkgs, ...}: {
   vim.languages.tailwind = {
     enable = true;
-    lsp.enable = true;
+    lsp.enable = false;
+  };
+
+  vim.extraPackages = [
+    pkgs.tailwindcss-language-server
+  ];
+
+  vim.lsp.servers.tailwindcss = {
+    enable = true;
+    filetypes = [
+      "html"
+      "css"
+      "scss"
+      "javascript"
+      "javascriptreact"
+      "typescript"
+      "typescriptreact"
+      "svelte"
+    ];
+    settings = {
+      tailwindCSS = {
+        lint = {
+          cssConflict = "warning";
+          invalidApply = "error";
+          invalidConfigPath = "error";
+          invalidScreen = "error";
+          invalidTailwindDirective = "error";
+          invalidVariant = "error";
+          recommendedVariantOrder = "warning";
+        };
+        experimental = {
+          classRegex = [
+            ["([\"'`][^\"'`]*.*?[\"'`])" "[\"'`]([^\"'`]*).*?[\"'`]"] # Tailwind Variants
+          ];
+        };
+      };
+    };
   };
 }
