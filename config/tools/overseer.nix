10d2481cd7860ab995dffdbed19b260d2e311efe
Jaren Glenn <jarenglenn@gmail.com>
2025-09-05 15:33:17 -0600

    fix(tools/overseer): add OverseerRestartLast user command

--- config/tools/overseer.nix
+++ config/tools/overseer.nix
@@ -73,65 +73,95 @@ in {
         mode = ["n" "x" "v"];
         desc = "Restart Last Task";
         lua = true;
-        action = ''
-          function()
-            local overseer = require("overseer")
-            vim.cmd("OverseerRestartLast")
+        action =
+          /*
+          lua
+          */
+          ''
+            function()
+              local overseer = require("overseer")
+              vim.cmd("OverseerRestartLast")
 
-            -- Don't do anything if there are no tasks
-            local status = { overseer.STATUS.RUNNING, overseer.STATUS.PENDING }
-            local most_recent = overseer.list_tasks({ status = status, recent_first = true })[1]
-            if not most_recent then
-              return
+              -- Don't do anything if there are no tasks
+              local status = { overseer.STATUS.RUNNING, overseer.STATUS.PENDING }
+              local most_recent = overseer.list_tasks({ status = status, recent_first = true })[1]
+              if not most_recent then
+                return
+              end
+
+              vim.notify("Restarted last Overseer task: " .. most_recent.name, vim.log.levels.INFO, { title = "Overseer" })
             end
-
-            vim.notify("Restarted last Overseer task: " .. most_recent.name, vim.log.levels.INFO, { title = "Overseer" })
-          end
-        '';
+          '';
       }
     ];
   };
+
+  vim.luaConfigRC.overseer_restart_last =
+    /*
+    lua
+    */
+    ''
+      vim.api.nvim_create_user_command("OverseerRestartLast", function()
+        local overseer = require("overseer")
+        local tasks = overseer.list_tasks({ recent_first = true })
+        if vim.tbl_isempty(tasks) then
+          vim.notify("No tasks found", vim.log.levels.WARN)
+        else
+          overseer.run_action(tasks[1], "restart")
+        end
+      end, {})
+    '';
 
   vim.autocmds = [
     {
       event = ["User"];
       pattern = ["PersistenceSavePre"];
       desc = "Save Overseer tasks during Persistence session save";
-      callback = lib.generators.mkLuaInline ''
-        function()
-          local function get_cwd_as_name()
-            local dir = vim.fn.getcwd(0)
-            return dir:gsub("[^A-Za-z0-9]", "_")
-          end
+      callback =
+        lib.generators.mkLuaInline
+        /*
+        lua
+        */
+        ''
+          function()
+            local function get_cwd_as_name()
+              local dir = vim.fn.getcwd(0)
+              return dir:gsub("[^A-Za-z0-9]", "_")
+            end
 
-          local overseer = require("overseer")
-          overseer.save_task_bundle(get_cwd_as_name(), nil, { on_conflict = "overwrite" })
-        end
-      '';
+            local overseer = require("overseer")
+            overseer.save_task_bundle(get_cwd_as_name(), nil, { on_conflict = "overwrite" })
+          end
+        '';
     }
     {
       event = ["User"];
       pattern = ["PersistenceLoadPost"];
       desc = "Load Overseer tasks during Persistence session load";
-      callback = lib.generators.mkLuaInline ''
-        function()
-          local function get_cwd_as_name()
-            local dir = vim.fn.getcwd(0)
-            return dir:gsub("[^A-Za-z0-9]", "_")
-          end
+      callback =
+        lib.generators.mkLuaInline
+        /*
+        lua
+        */
+        ''
+          function()
+            local function get_cwd_as_name()
+              local dir = vim.fn.getcwd(0)
+              return dir:gsub("[^A-Za-z0-9]", "_")
+            end
 
-          local overseer = require("overseer")
-          overseer.load_task_bundle(get_cwd_as_name(), { ignore_missing = true })
+            local overseer = require("overseer")
+            overseer.load_task_bundle(get_cwd_as_name(), { ignore_missing = true })
 
-          -- List loaded tasks to notify user
-          local status = { overseer.STATUS.RUNNING, overseer.STATUS.PENDING }
-          local tasks = overseer.list_tasks({ status = status })
-          local message = "Restored " .. #tasks .. " Overseer task" .. (#tasks == 1 and "" or "s")
-          if #tasks > 0 then
-            vim.notify(message, vim.log.levels.INFO, { title = "Overseer" })
+            -- List loaded tasks to notify user
+            local status = { overseer.STATUS.RUNNING, overseer.STATUS.PENDING }
+            local tasks = overseer.list_tasks({ status = status })
+            local message = "Restored " .. #tasks .. " Overseer task" .. (#tasks == 1 and "" or "s")
+            if #tasks > 0 then
+              vim.notify(message, vim.log.levels.INFO, { title = "Overseer" })
+            end
           end
-        end
-      '';
+        '';
     }
   ];
 }
