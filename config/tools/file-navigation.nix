9bd74f66db5a2b26a41d0a8926a0e63563d7d81e
Jaren Glenn <jarenglenn@gmail.com>
2025-10-10 18:42:57 -0600

    feat: filter __pycache__ from mini.files

--- config/tools/file-navigation.nix
+++ config/tools/file-navigation.nix
@@ -8,6 +8,23 @@ in {
       setupOpts = {
         windows.preview = false;
         options.use_as_default_explorer = true;
+        content.filter =
+          lib.generators.mkLuaInline
+          /*
+          lua
+          */
+          ''
+            function(fs_entry)
+              local hidden = { "__pycache__" }
+
+              for _, pattern in ipairs(hidden) do
+                if string.match(fs_entry.name, pattern) then
+                  return false
+                end
+              end
+              return true
+            end
+          '';
       };
     };
   };
@@ -32,22 +49,27 @@ in {
       event = ["User"];
       pattern = ["MiniFilesActionDelete" "MiniFilesActionMove"];
       desc = "Update buffers after deleting or moving a file";
-      callback = lib.generators.mkLuaInline ''
-        function(args)
-          local action = args.data.action
-          local from = args.data.from
-          local to = args.data.to
+      callback =
+        lib.generators.mkLuaInline
+        /*
+        lua
+        */
+        ''
+          function(args)
+            local action = args.data.action
+            local from = args.data.from
+            local to = args.data.to
 
-          local bufnr = vim.fn.bufnr(from, true)
+            local bufnr = vim.fn.bufnr(from, true)
 
-          if bufnr ~= -1 then
-            require("mini.bufremove").delete(bufnr, true)
-            if action == "move" then
-              vim.fn.bufadd(to)
+            if bufnr ~= -1 then
+              require("mini.bufremove").delete(bufnr, true)
+              if action == "move" then
+                vim.fn.bufadd(to)
+              end
             end
           end
-        end
-      '';
+        '';
     }
     {
       event = ["User"];
