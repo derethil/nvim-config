4cb7f6054430e1eccca6d9a3b7ad3eef2dba684e
Jaren Glenn <jarenglenn@gmail.com>
2025-11-12 20:39:11 -0700

    fix(config/tools): fix diffview keymaps

--- config/tools/diffview.nix
+++ config/tools/diffview.nix
@@ -1,4 +1,4 @@
-{
+{lib, ...}: {
   vim.utility.diffview-nvim = {
     enable = true;
     setupOpts = {
@@ -14,10 +14,14 @@
         win_config.position = "right";
       };
       keymaps = let
-        mkDiffviewKeymap = mode: key: cmd: desc: [
+        mkDiffviewKeymap = mode: key: action: desc: [
           mode
           key
-          cmd
+          (
+            if lib.hasPrefix "<CMD>" action
+            then action
+            else (lib.generators.mkLuaInline action)
+          )
           {
             inherit desc;
             silent = true;
@@ -28,25 +32,25 @@
         view = [
           (mkDiffviewKeymap "n" "q" "<CMD>DiffviewClose<CR>" "Diffview: Quit")
 
-          (mkDiffviewKeymap "n" "[F" ''<CMD>lua require("diffview.actions").select_first_entry<CR>'' "Fist File")
-          (mkDiffviewKeymap "n" "]F" ''<CMD>lua require("diffview.actions").select_last_entry<CR>'' "Last File")
-          (mkDiffviewKeymap "n" "[f" ''<CMD>lua require("diffview.actions").select_prev_entry<CR>'' "Previous File")
-          (mkDiffviewKeymap "n" "]f" ''<CMD>lua require("diffview.actions").select_next_entry<CR>'' "Next File")
+          (mkDiffviewKeymap "n" "[F" ''require("diffview.actions").select_first_entry'' "First File")
+          (mkDiffviewKeymap "n" "]F" ''require("diffview.actions").select_last_entry'' "Last File")
+          (mkDiffviewKeymap "n" "[f" ''require("diffview.actions").select_prev_entry'' "Previous File")
+          (mkDiffviewKeymap "n" "]f" ''require("diffview.actions").select_next_entry'' "Next File")
 
-          (mkDiffviewKeymap "n" "[m" ''<CMD>lua require("diffview.actions").prev_conflict<CR>'' "Next Conflict")
-          (mkDiffviewKeymap "n" "]m" ''<CMD>lua require("diffview.actions").next_conflict<CR>'' "Previous Conflict")
+          (mkDiffviewKeymap "n" "[m" ''require("diffview.actions").prev_conflict'' "Previous Conflict")
+          (mkDiffviewKeymap "n" "]m" ''require("diffview.actions").next_conflict'' "Next Conflict")
 
-          (mkDiffviewKeymap "n" "<leader>mo" ''<CMD>lua require("diffview.actions").conflict_choose("ours")<CR>'' "Choose OURS")
-          (mkDiffviewKeymap "n" "<leader>mt" ''<CMD>lua require("diffview.actions").conflict_choose("theirs")<CR>'' "Choose THEIRS")
-          (mkDiffviewKeymap "n" "<leader>mb" ''<CMD>lua require("diffview.actions").conflict_choose("base")<CR>'' "Choose BASE")
-          (mkDiffviewKeymap "n" "<leader>ma" ''<CMD>lua require("diffview.actions").conflict_choose("all")<CR>'' "Choose ALL")
-          (mkDiffviewKeymap "n" "<leader>mx" ''<CMD>lua require("diffview.actions").conflict_choose("none")<CR>'' "Delete conflict region")
+          (mkDiffviewKeymap "n" "<leader>mo" ''require("diffview.actions").conflict_choose("ours")'' "Choose OURS")
+          (mkDiffviewKeymap "n" "<leader>mt" ''require("diffview.actions").conflict_choose("theirs")'' "Choose THEIRS")
+          (mkDiffviewKeymap "n" "<leader>mb" ''require("diffview.actions").conflict_choose("base")'' "Choose BASE")
+          (mkDiffviewKeymap "n" "<leader>ma" ''require("diffview.actions").conflict_choose("all")'' "Choose ALL")
+          (mkDiffviewKeymap "n" "<leader>mx" ''require("diffview.actions").conflict_choose("none")'' "Delete conflict region")
 
-          (mkDiffviewKeymap "n" "<leader>mO" ''<CMD>lua require("diffview.actions").conflict_choose_all("ours")<CR>'' "Choose OURS (whole file)")
-          (mkDiffviewKeymap "n" "<leader>mT" ''<CMD>lua require("diffview.actions").conflict_choose_all("theirs")<CR>'' "Choose THEIRS (whole file)")
-          (mkDiffviewKeymap "n" "<leader>mB" ''<CMD>lua require("diffview.actions").conflict_choose_all("base")<CR>'' "Choose BASE (whole file)")
-          (mkDiffviewKeymap "n" "<leader>mA" ''<CMD>lua require("diffview.actions").conflict_choose_all("all")<CR>'' "Choose ALL (whole file)")
-          (mkDiffviewKeymap "n" "<leader>mX" ''<CMD>lua require("diffview.actions").conflict_choose_all("none")<CR>'' "Delete conflict region (whole file)")
+          (mkDiffviewKeymap "n" "<leader>mO" ''require("diffview.actions").conflict_choose_all("ours")'' "Choose OURS (whole file)")
+          (mkDiffviewKeymap "n" "<leader>mT" ''require("diffview.actions").conflict_choose_all("theirs")'' "Choose THEIRS (whole file)")
+          (mkDiffviewKeymap "n" "<leader>mB" ''require("diffview.actions").conflict_choose_all("base")'' "Choose BASE (whole file)")
+          (mkDiffviewKeymap "n" "<leader>mA" ''require("diffview.actions").conflict_choose_all("all")'' "Choose ALL (whole file)")
+          (mkDiffviewKeymap "n" "<leader>mX" ''require("diffview.actions").conflict_choose_all("none")'' "Delete conflict region (whole file)")
         ];
         file_panel = [
           (mkDiffviewKeymap "n" "q" "<CMD>DiffviewClose<CR>" "Diffview: Quit")
