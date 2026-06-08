c2b9ac4cfec0b76136f09ed7a9bd1b9c98fd7dd8
Jaren Glenn <jarenglenn@gmail.com>
2026-06-08 16:03:21 -0600

    feat(languages): enable docker config

---
+++ config/languages/docker.nix
@@ -0,0 +1,17 @@
+{
+  vim.languages.docker = {
+    enable = true;
+    lsp = {
+      enable = true;
+    };
+    extraDiagnostics = {
+      enable = true;
+    };
+    treesitter = {
+      enable = true;
+    };
+    format = {
+      enable = true;
+    };
+  };
+}
