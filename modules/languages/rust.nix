555d3b4f07cdb4f535ac3aa1112e4b2e0a7d7174
Jaren Glenn <jarenglenn@gmail.com>
2026-07-09 14:25:27 -0600

    feat(languages): enable inlay hints in rust

--- modules/languages/rust.nix
+++ modules/languages/rust.nix
@@ -1,14 +1,52 @@
 {...}: {
-  flake.modules.nvf.languages-rust = {
+  flake.modules.nvf.languages-rust = {lib, ...}: {
     vim.languages.rust = {
       enable = true;
-      lsp.enable = true;
-      treesitter.enable = true;
-      format.enable = true;
-      dap.enable = true;
+
+      lsp = {
+        enable = true;
+        opts = ''
+          ['rust-analyzer'] = {
+            inlayHints = {
+              lifetimeElisionHints = { enable = "always" },
+              bindingModeHints = { enable = true },
+            },
+          },
+        '';
+      };
+
+      treesitter = {
+        enable = true;
+      };
+
+      format = {
+        enable = true;
+      };
+      dap = {
+        enable = true;
+      };
+
       extensions = {
-        crates-nvim.enable = true;
+        crates-nvim = {
+          enable = true;
+        };
       };
     };
+
+    vim.autocmds = [
+      {
+        event = ["LspAttach"];
+        pattern = ["*.rs"];
+        desc = "Enable inlay hints for Rust";
+        callback = lib.generators.mkLuaInline ''
+          function(args)
+            local client = vim.lsp.get_client_by_id(args.data.client_id)
+            if client and client.server_capabilities.inlayHintProvider then
+              vim.lsp.inlay_hint.enable(true, { bufnr = args.buf })
+            end
+          end
+        '';
+      }
+    ];
   };
 }
