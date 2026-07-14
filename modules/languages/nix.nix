a00b92f69f0ec07c616d6fc05a50c5a51a011bd6
Jaren Glenn <jarenglenn@gmail.com>
2026-07-13 22:05:55 -0600

    feat(nix): switch to nixd lsp

--- modules/languages/nix.nix
+++ modules/languages/nix.nix
@@ -2,11 +2,23 @@
   flake.modules.nvf.languages-nix = {
     vim.languages.nix = {
       enable = true;
-      lsp.enable = true;
-      treesitter.enable = true;
+      lsp = {
+        enable = true;
+        servers = ["nixd"];
+      };
+      treesitter = {
+        enable = true;
+      };
       format = {
         enable = true;
       };
+      extraDiagnostics = {
+        enable = true;
+      };
+    };
+
+    vim.lsp = {
+      presets.nixd.enable = true;
     };
   };
 }
