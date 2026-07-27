d37d93a5e7d0355ef8e96d10f83bc20abc1d0727
Jaren Glenn <jarenglenn@gmail.com>
2026-07-27 15:13:05 -0600

    revert: unpin tsgo

--- flake/nixpkgs.nix
+++ flake/nixpkgs.nix
@@ -6,8 +6,6 @@
   flake-file.inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-25.11";
-    # Pinned to the last nixpkgs commit before typescript-go became unstable (2026-04-24 build)
-    nixpkgs-tsgo.url = "github:NixOS/nixpkgs/6368eda62c9775c38ef7f714b2555a741c20c72d";
   };
 
   flake.lib.mkOverlayedPkgs = system: let
@@ -22,12 +20,7 @@
       inherit system config;
     };
 
-    pkgs-tsgo = import inputs.nixpkgs-tsgo {
-      inherit system config;
-    };
-
     bootstrapOverlay = _final: _prev: {
-      inherit (pkgs-tsgo) typescript-go;
       stable = pkgs-stable;
     };
   in
