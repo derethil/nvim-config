cbe423be5584068223df80ca5b3ea2efad9758c8
Jaren Glenn <jarenglenn@gmail.com>
2026-08-10 00:07:05 -0600

    feat: switch to tombi

--- modules/languages/toml.nix
+++ modules/languages/toml.nix
@@ -2,9 +2,22 @@
   flake.modules.nvf.languages-toml = {
     vim.languages.toml = {
       enable = true;
-      extraDiagnostics.enable = true;
-      format.enable = true;
-      lsp.enable = true;
+
+      extraDiagnostics = {
+        enable = true;
+        types = ["tombi"];
+      };
+
+      format = {
+        enable = true;
+        type = ["tombi"];
+      };
+
+      lsp = {
+        enable = true;
+        servers = ["tombi"];
+      };
+
       treesitter.enable = true;
     };
   };
